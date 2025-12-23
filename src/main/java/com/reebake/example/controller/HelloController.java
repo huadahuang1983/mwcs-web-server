b51234b28527b7c2df4f21e2033a0a89b1ac8272
彩花科技 <huadahuang1983@163.com>
2025-12-24 01:21:25 +0800

    feat: 补充

--- src/main/java/com/reebake/example/controller/HelloController.java
+++ src/main/java/com/reebake/example/controller/HelloController.java
@@ -1,13 +1,22 @@
 package com.reebake.example.controller;
 
+import cn.hutool.core.lang.Dict;
+import com.reebake.mwcs.infra.mapper.DataDictMapper;
+import com.reebake.mwcs.message.mapper.MessageTemplateMapper;
+import com.reebake.mwcs.storage.mapper.FileDetailMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@RequiredArgsConstructor
 public class HelloController {
-    
+    private final DataDictMapper dataDictMapper;
+    private final FileDetailMapper fileDetailMapper;
+    private final MessageTemplateMapper messageTemplateMapper;
+
     @GetMapping("/hello")
     public String hello() {
         return "Hello, Spring Boot 3.5.8!";
